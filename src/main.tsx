2471e9de2efa27f0b6dc0f5f101d74e964afd889
Pikj Reyderman <pikj.reyderman@gmail.com>
2024-02-27 19:28:30 +0200

    :upd

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './routes/root.tsx'
-import '@/assets/styles/App.css'
+import './assets/styles/App.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
